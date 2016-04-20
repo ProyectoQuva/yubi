2a2a37e694542b0570a73afd0d5ce5100e72868d
Jonas Diaz <jonas.diaz0@gmail.com>
2016-04-19 22:18:38 -0300

    Estructura de ratas, controlodares, servicios

---
+++ www/js/controllers.js
@@ -0,0 +1,7 @@
+angular.module('yubi.controllers', ['yubi.services'])
+
+.controller('HomeCtrl', function(){
+	$scope.search = function(){
+
+	};
+});
---
+++ www/js/services.js
@@ -0,0 +1,5 @@
+angular.module('yubi.services', [])
+
+.factory('Search', function(){
+	
+})
